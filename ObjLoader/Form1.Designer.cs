5a5e9329c34f9ccb5925f67be492491d678e5e28
Andrew MacGinitie <macginitie@charter.net>
2018-09-23 16:54:09 -0400

    1st baby steps

--- ObjLoader/Form1.Designer.cs
+++ ObjLoader/Form1.Designer.cs
@@ -28,13 +28,142 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.btnLoadFile = new System.Windows.Forms.Button();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtObjFile = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.progressBar1);
+            this.panel1.Controls.Add(this.btnLoadFile);
+            this.panel1.Controls.Add(this.btnBrowse);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.txtObjFile);
+            this.panel1.Location = new System.Drawing.Point(9, 11);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(783, 95);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(642, 56);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Cancel Loading";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(244, 56);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(391, 23);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // btnLoadFile
+            // 
+            this.btnLoadFile.Location = new System.Drawing.Point(106, 56);
+            this.btnLoadFile.Name = "btnLoadFile";
+            this.btnLoadFile.Size = new System.Drawing.Size(131, 27);
+            this.btnLoadFile.TabIndex = 3;
+            this.btnLoadFile.Text = "&Load File";
+            this.btnLoadFile.UseVisualStyleBackColor = true;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBrowse.AutoSize = true;
+            this.btnBrowse.Location = new System.Drawing.Point(641, 19);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(61, 23);
+            this.btnBrowse.TabIndex = 2;
+            this.btnBrowse.Text = "&Browse...";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.BtnBrowse_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(57, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = ".obj File";
+            // 
+            // txtObjFile
+            // 
+            this.txtObjFile.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtObjFile.Location = new System.Drawing.Point(106, 21);
+            this.txtObjFile.Name = "txtObjFile";
+            this.txtObjFile.Size = new System.Drawing.Size(529, 20);
+            this.txtObjFile.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.Location = new System.Drawing.Point(9, 112);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(783, 296);
+            this.panel2.TabIndex = 1;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(719, 418);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(54, 21);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.Text = "&Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.BtnClose_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = ".obj File Info Viewer";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtObjFile;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button btnLoadFile;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
     }
 }
 
--- ObjLoader/Form1.cs
+++ ObjLoader/Form1.cs
@@ -12,9 +12,35 @@ namespace ObjLoader
 {
     public partial class Form1 : Form
     {
+        string _objFilePath;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void BtnBrowse_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (DialogResult.OK == result)
+            {
+                _objFilePath = openFileDialog1.FileName;
+                txtObjFile.Text = _objFilePath;
+            }
+        }
+
+        private bool OK2Close()
+        {
+            // just return true for now
+            return true;
+        }
+
+        private void BtnClose_Click(object sender, EventArgs e)
+        {
+            if (OK2Close())
+            {
+                Application.Exit();
+            }
         }
     }
 }
